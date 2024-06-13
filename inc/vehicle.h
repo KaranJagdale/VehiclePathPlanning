4abb28f80cd3571829519730e1f9febac0242580
Karan Jagdale <karanjagdale1@gmail.com>
2024-06-13 00:51:27 -0700

    started adding the vehicle header file

---
+++ inc/vehicle.h
@@ -0,0 +1,14 @@
+// This file contains vehicle class. As of now it will implement simple bicycle model
+
+class Vehicle{
+private:
+    float wheel_base;
+    float mass;
+public:
+    Vehicle(vehicle_wheel_base, vehicle_mass)
+    {
+        wheel_base = vehicle_wheel_base; 
+        mass = vehicle_mass;
+    }
+
+};
