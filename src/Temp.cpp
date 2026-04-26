e6ffac6d2b5f674885e043281f3e43b46c89387d
Karan Jagdale <karanjagdale1@gmail.com>
2026-04-26 15:50:38 -0700

    cleaned up the repo

--- src/Temp.cpp
+++ src/Temp.cpp
@@ -1,24 +1,10 @@
 #include <iostream>
-#include <cmath>
-#include <Eigen/Dense>
-#include <vector>
-#include <functional>
-#include "../inc/spline.h"
-#include "../inc/solver.h"
+
+
 
 int main()
 {
-    Eigen::Matrix2f mat{{1,2},{3,4}};
-    Eigen::Vector2f vec{1, 0};
-
-    float a = 1;
-    if(true)
-    {
-        a = 2;
-    }
-    std::cout << a << std::endl;
-
-    return 0;
+    std::cout << "c++ version - " << __cplusplus << std::endl;
 
 }
     
